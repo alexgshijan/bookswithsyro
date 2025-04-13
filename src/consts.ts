46f2699f8ecb422c1237abebfa3d05f5ad889fe2
BlancOfficial <alexgshijan@gmail.com>
2025-04-13 17:25:24 +0100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Books With Syro";
+export const SITE_DESCRIPTION = "Books With Syro, an archive of digitial materials as part of the unofficial digital evangelisation of the Syro-Malabar Church";
